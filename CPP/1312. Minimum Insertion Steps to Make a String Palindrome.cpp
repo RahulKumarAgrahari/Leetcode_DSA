e75304a1b1a86067c73c7640c796bd9011f58e1c
Rahul <126974555+RahulKumarAgrahari@users.noreply.github.com>
2025-09-28 16:48:13 +0530

    Create 1312. Minimum Insertion Steps to Make a String Palindrome.cpp
    Min Insertions = s.length() - LongestPalindromicSubsequence(s);

---
+++ CPP/1312. Minimum Insertion Steps to Make a String Palindrome.cpp
@@ -0,0 +1,27 @@
+class Solution {
+private:
+    int lcs(string& s, string& t)   {
+        int n1 = s.size(), n2 = t.size();        
+        vector<int> prev(n2+1, 0), cur(n2+1, 0);
+        // shifting the indices by +1 to resolve the -1 indices denoting empty strings
+
+        for(int i=1; i<=n1; ++i) {            
+            for(int j=1; j<=n2; ++j)  {
+                if(s[i-1] == t[j-1])            // Here, s[i-1] & t[j-1] to compensate shifting
+                    cur[j] = 1 + prev[j-1];
+                else                
+                    cur[j] = max(cur[j-1], prev[j]);
+            }
+            prev = cur;
+        }
+        return prev[n2];
+    }
+
+public:
+    int minInsertions(string s) {
+        string t = s;
+        reverse(t.begin(), t.end());
+
+        return (s.size() - lcs(s,t));
+    }
+};
